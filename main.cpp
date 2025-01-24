09bd748dc97189b50ebe3add4b50807ac5c378f9
moonlightwiz <mittalshaurya50@gmail.com>
2025-01-24 23:49:08 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@
 #include<iterator>
 #include<climits>
 
-
 using namespace std;
 
 long long mod=1e9 +7;
+vector<long long int>factorial;
+vector<long long int>invfactorial;
 
 int main() {
     
@@ -20,79 +21,60 @@ int main() {
     cin.tie(0);
     cout.tie(0);
     
-    int t;
-    cin>>t;
-    while(t--)
+    int n;
+    cin>>n;
+    factorial.resize(1e6+1);
+    invfactorial.resize(1e6+1);
+    factorial[0]=1;
+    for(int i=1;i<=1e6;i++)
     {
-        int n;
-        cin>>n;
-        vector<int>v(n+1);
-        vector<vector<int>>adj(n+1);
-        for(int i=1;i<n;i++)
+        factorial[i]=((factorial[i-1]%mod)*(i%mod))%mod;
+    }
+//    for(int i=0;i<11;i++)
+//    {
+//        cout<<factorial[i]<<" ";
+//    }
+    //cout<<"\n";
+    invfactorial[0]=1;
+    for(int i=1;i<=1e6;i++)
+    {
+        long long temp=1;
+        long long curr=factorial[i];
+        for(int j=0;j<32;j++)
         {
-            int a,b;
-            cin>>a>>b;
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-            v[a]++;
-            v[b]++;
-        }
-        int maxi=1;
-        for(int i=2;i<=n;i++)
-        {
-            if(v[i]>v[maxi])
+            if((1<<j)&(mod-2))
             {
-                maxi=i;
+                temp*=curr;
+                temp%=mod;
             }
+            curr*=curr;
+            curr%=mod;
         }
-        int curr=v[maxi];
-        cout<<"maxi 1 : "<<maxi<<endl;
-        cout<<"curr after 1 round : "<<curr<<"\n";
-        for(auto i:adj[maxi])
+        invfactorial[i]=temp;
+    }
+    
+    if(n==1)
+    {
+        cout<<0;
+        return 0;
+    }
+    
+    bool counter=0;
+    long long temp=0;
+    for(int i=2;i<=n;i++)
+    {
+        cout<<temp<<" ";
+        if(counter)
         {
-            v[i]--;
-        }
-        
-        int maxi2=-1;
-        for(int i=1;i<maxi;i++)
-        {
-            if(maxi2==-1)
-            {
-                maxi2=i;
-            }
-            else{
-                if(v[i]>v[maxi2])
-                {
-                    maxi2=i;
-                }
-            }
-        }
-        for(int i=maxi+1;i<=n;i++)
-        {
-            if(maxi2==-1)
-            {
-                maxi2=i;
-            }
-            else{
-                if(v[i]>v[maxi2])
-                {
-                    maxi2=i;
-                }
-            }
-        }
-        
-        //cout<<"maxi 2 : "<<maxi2<<"\n";
-        //cout<<"v[maxi2] : "<<v[maxi2]<<"\n";
-        if(v[maxi2]==0)
-        {
-            curr--;
+            temp-=((factorial[n]%mod)*(invfactorial[i]%mod))%mod;
         }
         else{
-            curr+=v[maxi2];
-            curr--;
+            temp+=((factorial[n]%mod)*(invfactorial[i]%mod))%mod;
         }
-        cout<<curr<<"\n";
+        temp%=mod;
+        counter=!counter;
     }
+    cout<<temp<<" ";
     
     return 0;
 }
